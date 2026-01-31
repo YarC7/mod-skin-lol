5caeffe33fa335e32893b158a9cfe3dc246ce082
Cankkun <nguyenduccanh177@gmail.com>
2026-01-31 10:25:17 +0700

    fix: correct typo in forge.config.ts

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,7 @@ import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
-import { PublisherGithub } from '@electron-forge/publisher-github';
+import { PublisherGithub   } from '@electron-forge/publisher-github';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -33,7 +33,7 @@ const config: ForgeConfig = {
     }),
   ],
   publishers: [
-    new PublishBrowserWindowerGithub({
+    new PublisherGithub({
       repository: {
         owner: 'YarC7',
         name: 'mod-skin-lol',
